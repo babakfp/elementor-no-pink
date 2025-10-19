10acec51db26b5b66b7bd578345fd78d75d1c16a
babakfp <babak.bxf@gmail.com>
2025-10-19 19:01:06 +0330

    remove changelog, update versions

--- elementor-better-ui.php
+++ elementor-better-ui.php
@@ -2,27 +2,28 @@
 
 /**
  * Plugin Name:                       Elementor Better UI
- * Version:                           0.0.3
+ * Version:                           0.0.4
  * Description:                       Enhances Elementor UI design and user experience.
  * Author:                            babakfp
  * Author URI:                        https://babakfp.ir
  * Plugin URI:                        https://www.example.com/my-plugin
- * Tested up to:                      6.2.1
- * Tested up to (PHP):                7.4.33
- * Tested up to (Elementor):          3.13.2
- * Requires at least:                 x.x.x
- * Requires at least (PHP):           x.x.x
- * Requires at least (Elementor):     x.x.x
+ * Tested up to:                      6.8
+ * Requires at least:                 5.0
+ * Requires PHP:                      7.4
+ * PHP tested up to:                  8.4
+ * Elementor tested up to:            3.32
+ * Elementor requires at least:       3.32
  * License:                           GPLv3 or later
  * License URI:                       https://www.gnu.org/licenses/gpl-3.0.html
  * Text Domain:                       elementor-better-ui
  * Domain Path:                       /languages
  * Tags:                              elementor, better, ui
+ * Requires Plugins                   elementor
  */
 
 if ( ! class_exists( 'Elementor_Better_UI' ) ) {
 	final class Elementor_Better_UI {
-		public static $version = '0.0.3';
+		public static $version = '0.0.4';
 
 		public function __construct() {
             add_action( 'admin_enqueue_scripts', fn() => wp_enqueue_style( 'elementor-better-ui:wp-admin', self::url() . 'static/css/wp-admin.css', [], self::$version ), 11 );

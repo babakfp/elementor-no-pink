d43c769122f0cd57ee79f5c804f37cc1bd47fe53
babakfp <babak.bxf@gmail.com>
2025-10-19 18:15:34 +0330

    fix colors for wp-admin

--- elementor-better-ui.php
+++ elementor-better-ui.php
@@ -25,7 +25,7 @@ if ( ! class_exists( 'Elementor_Better_UI' ) ) {
 		public static $version = '0.0.3';
 
 		public function __construct() {
-            add_action( 'admin_enqueue_scripts', fn() => wp_enqueue_style( 'elementor-better-ui:wp-admin', self::url() . 'static/css/wp-admin.css', [], self::$version ) );
+            add_action( 'admin_enqueue_scripts', fn() => wp_enqueue_style( 'elementor-better-ui:wp-admin', self::url() . 'static/css/wp-admin.css', [], self::$version ), 11 );
             add_action( 'elementor/editor/after_enqueue_styles', fn() => wp_enqueue_style ( 'elementor-better-ui:el-editor', self::url() . 'static/css/el-editor.css', [], self::$version ) );
             add_action( 'elementor/preview/enqueue_styles', fn() => wp_enqueue_style ( 'elementor-better-ui:el-preview', self::url() . 'static/css/el-preview.css', [], self::$version ) );
 		}
